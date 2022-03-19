7db8b7f26017d22b6e1b642cec62ffe279f28417
Ema Čampa <emacampa3@gmail.com>
2022-03-19 13:30:34 +0100

    adding second hour hand

---
+++ script.js
@@ -0,0 +1,56 @@
+(function () {
+  var now = new Date(),
+    hourDeg = (now.getHours() / 12) * 360 + (now.getMinutes() / 60) * 30,
+    minuteDeg = (now.getMinutes() / 60) * 360 + (now.getSeconds() / 60) * 6,
+    secondDeg = (now.getSeconds() / 60) * 360,
+    stylesDeg = [
+      "@-webkit-keyframes rotate-hour{from{transform:rotate(" +
+        hourDeg +
+        "deg);}to{transform:rotate(" +
+        (hourDeg + 360) +
+        "deg);}}",
+      "@-webkit-keyframes rotate-minute{from{transform:rotate(" +
+        minuteDeg +
+        "deg);}to{transform:rotate(" +
+        (minuteDeg + 360) +
+        "deg);}}",
+      "@-webkit-keyframes rotate-second{from{transform:rotate(" +
+        secondDeg +
+        "deg);}to{transform:rotate(" +
+        (secondDeg + 360) +
+        "deg);}}",
+      "@-moz-keyframes rotate-hour{from{transform:rotate(" +
+        hourDeg +
+        "deg);}to{transform:rotate(" +
+        (hourDeg + 360) +
+        "deg);}}",
+      "@-moz-keyframes rotate-minute{from{transform:rotate(" +
+        minuteDeg +
+        "deg);}to{transform:rotate(" +
+        (minuteDeg + 360) +
+        "deg);}}",
+      "@-moz-keyframes rotate-second{from{transform:rotate(" +
+        secondDeg +
+        "deg);}to{transform:rotate(" +
+        (secondDeg + 360) +
+        "deg);}}",
+    ].join("")
+
+  document.getElementById("clock-animations").innerHTML = stylesDeg
+})()
+
+
+/* Day of the month */
+calendarDay = document.getElementById("calendar-day")
+
+const clock = () => {
+	let date = new Date()
+
+	let calDay = date.getDate()
+	calendarDay.innerHTML = calDay
+}
+
+setInterval(
+	clock,
+	1000
+)
